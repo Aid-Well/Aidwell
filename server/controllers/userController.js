a43114b133bc79cf85d4e529c0ab0b380f3a1558
Justinlkirk <justinleekirk@gmail.com>
2022-03-15 14:11:20 -0500

    Add User route is working

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,8 +1,37 @@
-const db = require('../model');
+/* eslint-disable consistent-return */
+const db = require('../models/userModel');
 
 const userController = {
-  updateFav() {
+  findUser(req, res, next) {
+
+  },
+
+  addUser(req, res, next) {
+    const noEmail = !('email' in req.body);
+    const noUsername = !('username' in req.body);
+    const noPassword = !('password' in req.body);
+    if (noEmail || noPassword || noUsername)
+      console.log('here')
+    return next({ status: 400, log: 'Missing credentials' });
+
+    db.create(
+      {
+        email: 'me@gmail.com',
+        username: 'me',
+        password: '1234',
+        charities: [],
+      },
+      (err, newUser) => {
+        if (err) return next(err);
+        res.locals.user = newUser;
+        return next();
+      }
+    );
+  },
+
+  updateFav(req, res, next) {
     console.log('updating fave');
+    return next();
   },
 };
 
--- server/routes/user.js
+++ server/routes/user.js
@@ -3,6 +3,10 @@ const userController = require('../controllers/userController');
 
 const router = express.Router();
 
+router.post('/addUser', userController.addUser, (req, res) =>
+  res.sendStatus(200)
+);
+
 router.post('/makeAD', (req, res) => res.sendStatus(200));
 
 router.put('/changeFav', (req, res) => res.sendStatus(200));
