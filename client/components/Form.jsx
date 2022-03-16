521596b668d89eea7c986e3a0b4ca62e38cb0678
nicoflrs <floresni1996@gmail.com>
2022-03-16 00:15:49 -0700

    Successfully displayed raw data from the charities API to the main page by updating the Redux store after API fetch.

--- client/components/Form.jsx
+++ client/components/Form.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import store from '../store.js';
-import { Link } from "react-router-dom";
 import { getCharitiesServ } from '../reducers/reducer.js';
 import axios from 'axios';
 
@@ -9,9 +8,10 @@ const Form = (props) =>{
 const createBody = () => {
   return {
     search: document.getElementById('search').value,
-    fundraisingOrgs: document.getElementById('fundraisingOrgs').value,
     state: document.getElementById('State').value,
+    city: document.getElementById('City').value,
     zip: document.getElementById('Zipcode').value,
+    fundraisingOrgs: document.getElementById('fundraisingOrgs').value,
     sizeRange: document.getElementById('size').value,
     donorPrivacy: document.getElementById('donorPrivacy').value,
     scopeOfWork: document.getElementById('scopeofwork').value,
@@ -21,25 +21,17 @@ const createBody = () => {
 
 const resetFields = () => {
   document.getElementById('search').value = '';
-  document.getElementById('fundraisingOrgs').value = '';
   document.getElementById('State').value = '';
+  document.getElementById('City').value = '';
   document.getElementById('Zipcode').value = '';
+  document.getElementById('fundraisingOrgs').value = '';
   document.getElementById('size').value = '';
   document.getElementById('donorPrivacy').value = '';
   document.getElementById('scopeofwork').value = '';
+  document.getElementById('noGovSupport').value = '';
 }
 
 const makeServerCall = (reqBody) => {
-  // const requestOptions = {
-  //     method: 'PUT',
-  //     headers: { 'Content-Type': 'application/json' },
-  //     body: reqBody,
-  // };
-  // fetch('/main/findCharities', requestOptions)
-  // // console.log('res before json: ', res)
-  // .then(res => res.json())
-  // // console.log(res);
-  // .then(res => console.log(res))
   axios({
     method: 'PUT',
     url: '/main/findCharities',
@@ -47,37 +39,23 @@ const makeServerCall = (reqBody) => {
     headers: { 'Content-Type': 'application/json' },
     data: reqBody
   })
-  .then(data => console.log('successful account creation!:', data))
+  .then(data => store.dispatch(getCharitiesServ(data)))
   .catch(err => console.log('err:', err))
 }
-
-const closeForm = () => props.setPopup(false);
     
 const handleSubmit = (el) => {
   el.preventDefault();
   const body = createBody();
-  console.log(body);
   resetFields();
   makeServerCall(body);
-  closeForm();
 }
-
-
 
 return(
   <div>
     <form onSubmit = {handleSubmit}>
-        <label htmlFor = 'search'>Keyword of Charity you are looking for: </label>
-        <input type="text" id="search" name="search" />
-        
-        <div className = 'dropdown'>
-          <label htmlFor = "fundraisingOrgs">Fundraising for Charities: </label>
-          <select name = "fundraisingOrgs" id = "fundraisingOrgs">
-          <option value="true">true</option>
-          <option value="false">false</option>
-          </select>
-        </div>
-
+        <label htmlFor = 'search'>Charity Keyword: </label>
+        <input type="text" id="search" name="search" /><br/>
+      
         <label htmlFor = 'State'>State: </label>
         <input type="text" id="State" name="State" /><br/>
 
@@ -88,11 +66,21 @@ return(
         <input type="text" id="Zipcode" name="Zipcode" /><br/>
 
         <div className = 'dropdown'>
+          <label htmlFor = "fundraisingOrgs">Fundraising for Charities: </label>
+          <select name = "fundraisingOrgs" id = "fundraisingOrgs">
+          <option disabled selected value> </option>
+          <option value="true">true</option>
+          <option value="false">false</option>
+          </select>
+        </div>
+
+        <div className = 'dropdown'>
           <label htmlFor = "size">Size: </label>
           <select name = "size" id = "size">
-          <option value="small">small</option>
-          <option value="medium">medium</option>
-          <option value='large'>large</option>
+          <option disabled selected value> </option>
+          <option value="small">Small</option>
+          <option value="medium">Medium</option>
+          <option value='large'>Large</option>
           </select>
         </div>
 
@@ -100,6 +88,7 @@ return(
         <div className = 'dropdown'>
           <label htmlFor = "donorPrivacy">Donor Privacy: </label>
           <select name = "donorPrivacy" id = "donorPrivacy">
+          <option disabled selected value> </option>
           <option value="true">true</option>
           <option value="false">false</option>
           </select>
@@ -108,23 +97,23 @@ return(
         <div className = 'dropdown'>
           <label htmlFor = "scopeofwork" >Scope of Work: </label>
           <select name = "scopeofwork" id = "scopeofwork">
-          <option value="All">All </option>
-          <option value="Regional">Regional</option>
-          <option value="National">National</option>
-          <option value="International">International</option>
+          <option disabled selected value> </option>
+          <option value="ALL">All </option>
+          <option value="REGIONAL">Regional</option>
+          <option value="NATIONAL">National</option>
+          <option value="INTERNATIONAL">International</option>
           </select>
         </div>
       
         <div className = 'dropdown'>
           <label htmlFor = "noGovSupport">Government Support: </label>
           <select name = "noGovSupport" id = "noGovSupport">
+          <option disabled selected value> </option>
           <option value="true">true</option>
           <option value="false">false</option>
           </select>
         </div>
-        {/* <Link to ='SearchResults'> */}
         <button type = 'submit' className ='formSubmit'>Submit</button>
-        {/* </Link> */}
     </form>
   </div>
   )
